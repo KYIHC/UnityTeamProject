ba21e0cf407b4c0ee2f5f49e93dc8358e72a8eb0
JeongYeonLEE <lkw930@naver.com>
2024-07-12 18:47:20 +0900

    수정ㄱㄱㄱㄱㄱ

--- Assets/CW/Scripts/Character.cs
+++ Assets/CW/Scripts/Character.cs
@@ -7,7 +7,7 @@ using UnityEngine.AI;
 public class Character : MonoBehaviour
 {
 
-    public Camera mainCamera;
+    private Camera mainCamera;
 
     private Animator anim;
     private Vector3 CharacterMove;
@@ -15,43 +15,49 @@ public class Character : MonoBehaviour
 
     public bool isMove;
     public bool isAttack;
-    
 
 
-    public float Speed=5f;
-    
-    
+
+    public float Speed = 5f;
+
+
 
 
     private void Awake()
-    { 
+    {
+
+
+
         mainCamera = Camera.main;
 
         anim = GetComponentInChildren<Animator>();
         nav = GetComponent<NavMeshAgent>();
         nav.updateRotation = false;
-        
+
     }
     private void Update()
     {
-        if(Input.GetMouseButton(1))
+        if (Input.GetMouseButton(1))
         {
             RaycastHit hit;
 
-            if(Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition),out hit))
-
+            if (Physics.Raycast(GetComponent<Camera>().ScreenPointToRay(Input.mousePosition), out hit))
             {
-                setCharacterMove(hit.point);
+
+                if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out hit))
+                {
+                    setCharacterMove(hit.point);
+                }
             }
         }
         LookMoveDir();
 
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag=="monster"+"boss")
+        if (other.tag == "monster" + "boss")
         {
 
         }
@@ -60,34 +66,34 @@ public class Character : MonoBehaviour
     public void setCharacterMove(Vector3 charMove)
     {
         nav.SetDestination(charMove);
-        CharacterMove= charMove;
+        CharacterMove = charMove;
         isMove = true;
-        anim.SetBool("isMove",true);
+        anim.SetBool("isMove", true);
 
     }
 
     public void LookMoveDir()
     {
-        if(isMove==true)
+        if (isMove == true)
         {
-            if(nav.velocity.magnitude==0f)
+            if (nav.velocity.magnitude == 0f)
             {
                 isMove = false;
                 anim.SetBool("isMove", false);
-                return;                
+                return;
             }
-            var dir = new Vector3(nav.steeringTarget.x,transform.position.y,nav.steeringTarget.z) - transform.position;
+            var dir = new Vector3(nav.steeringTarget.x, transform.position.y, nav.steeringTarget.z) - transform.position;
             dir.y = 0;
             anim.transform.forward = dir;
-            
-            
-            
 
-            
+
+
+
+
 
 
         }
-        
+
     }
 
 
