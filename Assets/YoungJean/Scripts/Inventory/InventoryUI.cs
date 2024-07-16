61b93bb480d455e9f9e996f7ef16ce83d9d69218
YoungJeanKim <koj887174@gmail.com>
2024-07-16 21:17:01 +0900

    풀,커,푸

--- Assets/YoungJean/Scripts/Inventory/InventoryUI.cs
+++ Assets/YoungJean/Scripts/Inventory/InventoryUI.cs
@@ -1,16 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
+    InventoryManager inventoryManager;
     public GameObject inventoryPanel;
     bool activeInventory = false;
 
+    public Slot[] slots;
+    public Transform slotHolder; 
+
     private void Start()
     {
-     inventoryPanel.SetActive(activeInventory);   
+        inventoryManager = InventoryManager.instance;
+        slots = slotHolder.GetComponentsInChildren<Slot>();
+        inventoryManager.onSlotCountChange += SlotChange;
+        inventoryPanel.SetActive(activeInventory);   
     }
+
+    private void SlotChange(int val)
+    {
+        for(int i = 0; i < slots.Length; i++)
+        {
+            if(i < inventoryManager.SlotCount)
+            {
+                slots[i].GetComponent<Button>().interactable = true;    
+            }
+            else
+            {
+                slots[i].GetComponent<Button>().interactable = false;
+            }
+        }
+    }
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.I))
@@ -18,5 +43,10 @@ public class InventoryUI : MonoBehaviour
             activeInventory = !activeInventory;
             inventoryPanel.SetActive(activeInventory);
         }
+    }
+
+    public void addSlot()
+    {
+        inventoryManager.SlotCount++;
     }
 }
