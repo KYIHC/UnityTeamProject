c0edd5fe35596327fb7d36cc87781b55f47ef59a
lchw <lchw9662@naver.com>
2024-07-23 21:27:50 +0900

    no message

--- Assets/CW/Scripts/PaticleManager.cs
+++ Assets/CW/Scripts/PaticleManager.cs
@@ -4,12 +4,46 @@ using UnityEngine;
 
 public class PaticleManager : MonoBehaviour
 {
-    public GameObject[] paticle;
+    public GameObject normalAttackpaticle;
 
-    public void normalAttackPaticle()
+    private int poolSize = 100;
+
+    private List<GameObject> pools = new List<GameObject>();
+
+    private void Start()
     {
-        Instantiate(paticle[0]);
+        for(int i=0;i<poolSize;i++)
+        {
+            GameObject obj = Instantiate(normalAttackpaticle);
+            obj.SetActive(false);
+            pools.Add(obj);
+        }
     }
+
+    public GameObject GetObject(Vector3 position,Quaternion rotation)
+    {
+        foreach(var obj in pools)
+        {
+            if(!obj.activeInHierarchy)
+            {
+                obj.transform.position = position;
+                obj.transform.rotation = rotation;
+                obj.SetActive(true);
+                return obj;
+            }
+        }
+        return null;
+    }
+    public void ReturnObjectToPool(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+
+    
+
+
+
+    
 
 
 }
